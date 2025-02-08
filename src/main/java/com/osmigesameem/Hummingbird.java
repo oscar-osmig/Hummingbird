017953d37903b332e09465879763f880a7d21533
Osmig_Torres <torresosmig@gmail.com>
2025-02-08 03:01:13 -0800

    hehe

--- src/main/java/com/osmigesameem/Hummingbird.java
+++ src/main/java/com/osmigesameem/Hummingbird.java
@@ -1,8 +1,61 @@
 package com.osmigesameem;
 
-public class Hammingbird {
+import com.osmigesameem.helper_tools.HelperTools;
+
+import java.util.HashMap;
+
+public class Hummingbird {
+    static HashMap<String, String> questionDictionary = new HashMap<>();
 
     public static void run(){
-        
+        // currently initialize the hashmap
+        init();
+        // AI introduces itself and returns the first task
+        String task = HelperTools.introduceSelf();
+
+        switch (task){
+            case "a": case "A":
+                String answer = answerQuestion();
+                if(answer.equals("null")){
+                    System.out.println("\nSorry I don't know the answer to that.");
+                }else {
+                    System.out.println(answer);
+                }
+                break;
+
+            case "s": case "S":
+                questionUser();
+                break;
+
+            default:
+                System.out.println("\nI did not understand what you just typed...");
+
+        }
+
+    }
+
+    private static String answerQuestion() {
+        // getting user's question
+       String userQuestion = HelperTools.getAnswer("What is your question? ");
+       // checking if question is on the 'questionDictionary'
+       if (questionDictionary.containsKey(userQuestion)){
+           String answer = questionDictionary.get(userQuestion);
+           return answer;
+       }else {
+           return "null";
+       }
+    }
+
+    private static void questionUser() {
+        String question = "What is 2 + 2? ";
+        String userAnswer = HelperTools.getAnswer(question);
+        questionDictionary.put(question, userAnswer);
+        System.out.println("Thank you for your answer.");
+        System.out.println(questionDictionary.toString());
+    }
+
+
+    private static void init(){
+        questionDictionary.put("Who are you?", "I'm Hummingbird");
     }
 }
--- src/main/java/com/osmigesameem/helper_tools/HelperTools.java
+++ src/main/java/com/osmigesameem/helper_tools/HelperTools.java
@@ -1,4 +1,21 @@
 package com.osmigesameem.helper_tools;
 
+import java.util.Scanner;
+
 public class HelperTools {
+
+    public static String introduceSelf(){
+        System.out.println(
+                "\nHello, I'm Hummingbird. I'm currently able to answer questions and ask you questions! " +
+                 "\n  Enter (a) to ask questions" +
+                 "\n  or    (s) to get questioned");
+        String userChoice = getAnswer("\nYour choice: ");
+        return userChoice;
+    }
+
+    public static String getAnswer(String q){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(q);
+        return scanner.nextLine();
+    }
 }
